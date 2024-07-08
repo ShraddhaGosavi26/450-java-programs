a4b2d781695f784524a8718d49d6f624c781fb10
Shraddha Gosavi <157584874+ShraddhaGosavi26@users.noreply.github.com>
2024-07-08 08:35:54 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,13 @@
+//import java.util.*;
+public class swap {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int temp;
+        System.out.println("Before Swapping value of a : "+a+" value of b: "+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After Swapping value of a : "+a+" value of b: "+b);
+    }
+}
